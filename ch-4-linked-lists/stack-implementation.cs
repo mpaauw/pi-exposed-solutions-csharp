2881529d4a5b0b834b35425edbc014351fc9b086
Matt <matt.paauw@gmail.com>
2017-06-12 18:50:15 -0700

    Added new implementation for question 4.1.

---
+++ ch-4-linked-lists/stack-implementation.cs
@@ -0,0 +1,79 @@
+public class Stack<T>
+{
+    private Node<T> top;
+    private int size;
+
+    public Stack(T top)
+    {
+        this.top = new Node<T>(top);
+        this.size++;
+    }
+
+    public void push(T value)
+    {
+        if (this.size < 1)
+        {
+            this.top = new Node<T>(value);
+            this.size++;
+        }
+        else
+        {
+            Node<T> newTop = new Node<T>(value);
+            newTop.setNext(this.top);
+            this.top = newTop;
+            this.size++;
+        }
+    }
+
+    public T peek()
+    {
+        return this.top.getData();
+    }
+
+    public T pop()
+    {
+        Node<T> oldTop = this.top;
+        this.top = this.top.getNext();
+        this.size--;
+        return oldTop.getData();
+    }
+
+    public int getSize()
+    {
+        return this.size;
+    }
+}
+
+public class Node<T>
+{
+
+    public Node(T data)
+    {
+        this.data = data;
+    }
+
+    private T data;
+
+    private Node<T> next;
+
+    public T getData()
+    {
+        return this.data;
+    }
+
+    public void setData(T data)
+    {
+        this.data = data;
+    }
+
+    public Node<T> getNext()
+    {
+        return this.next;
+    }
+
+    public void setNext(Node<T> next)
+    {
+        this.next = next;
+    }
+
+}
