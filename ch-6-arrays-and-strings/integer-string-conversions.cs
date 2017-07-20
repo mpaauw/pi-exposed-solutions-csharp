953c6f043a10bb79b05a5aa2433ca8ad40102741
Matt <matt.paauw@gmail.com>
2017-07-19 19:21:08 -0700

    Added refactored 'convertStringToInt' method for solution 6.4.

--- ch-6-arrays-and-strings/integer-string-conversions.cs
+++ ch-6-arrays-and-strings/integer-string-conversions.cs
@@ -19,40 +19,24 @@ public static string convertIntToString(int num)
     return builder.ToString();
 }
 
-public static int convertStringToInt(string str) {
-    int[] asciiMap = new int[129];
-    for(int i = 0; i < asciiMap.Length; i++) {
-        asciiMap[i] = -1;
-    }
+public static int convertStringToInt(string str)
+{
     char[] strCharArr = str.ToCharArray();
-    boolean signed = (strCharArr[0] == '-') ? true : false;
-    int size = 0;
-    if(signed) {
-        size = strCharArray.Length - 1;
-    }
-    else {
-        size = strCharArray.Length;
-    }
-    int power = 10 * (size - 1);
+    bool signed = (strCharArr[0] == '-') ? true : false;
     int finalInt = 0;
-    for(int i = 0; i < strCharArr.Length; i++) {
-        if(strCharArr[i] == '-') {
+    int power = 1;
+    for(int i = strCharArr.Length - 1; i >= 0; i--)
+    {
+        if(strCharArr[i] == '-')
+        {
             continue;
         }
-        int workingIndex = 0;
-        asciiMap[strCharArr[i]] = 0;
-        for(int k = 0; k < asciiMap.Length; k++) {
-            if(asciiMap[k] == 0) {
-                workingIndex = k;
-                asciiMap[k] = -1;
-                break;
-            }
-        }
-        int actualInt = workingIndex - 30;
+        int actualInt = strCharArr[i] - '0';
         finalInt += actualInt * power;
-        power /= 10;
+        power *= 10;
     }
-    if(signed) {
+    if (signed)
+    {
         finalInt *= -1;
     }
     return finalInt;
