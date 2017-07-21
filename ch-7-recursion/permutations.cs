37db378c2c6d65be934476e461bb3f5d39544ee0
Matt <matt.paauw@gmail.com>
2017-07-21 09:11:55 -0700

    Refactored solution 7.2.

--- ch-7-recursion/permutations.cs
+++ ch-7-recursion/permutations.cs
@@ -1,16 +1,16 @@
 public static void printPermutations(string str)
 {
-    permute(new bool[str.Length], new StringBuilder(), str.ToCharArray());
+    recurse(new bool[str.Length], new StringBuilder(), str.ToCharArray());
 }
 
-private static void permute(bool[] used, StringBuilder printer, char[] charArr)
+private static void recurse(bool[] used, StringBuilder printer, char[] charArr)
 {
     if (printer.Length == charArr.Length)
     {
         System.Diagnostics.Debug.WriteLine(printer);
         return;
     }
-    for (int i = 0; i < charArr.Length; ++i)
+    for (int i = 0; i < charArr.Length; i++)
     {
         if (used[i])
         {
@@ -18,7 +18,7 @@ private static void permute(bool[] used, StringBuilder printer, char[] charArr)
         }
         printer.Append(charArr[i]);
         used[i] = true;
-        permute(used, printer, charArr);
+        recurse(used, printer, charArr);
         used[i] = false;
         printer.Remove(printer.Length - 1, 1);
     }
