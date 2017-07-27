5386c91234f8e4774ed54560ee9ddef4a7697df2
Matt <matt.paauw@gmail.com>
2017-07-26 17:33:07 -0700

    Added various comments to solution 8.3.

--- ch-8-sorting/multi-key-sort.cs
+++ ch-8-sorting/multi-key-sort.cs
@@ -1,3 +1,4 @@
+// sort utilizes an 'Employee' class, which contains the custom rules for the multi-key sort based on which class attributes are chosen - and in which order - for the sort
 public static void sortMultiKey(List<Employee> employeeArr)
 {
     employeeArr.Sort();
@@ -9,6 +10,7 @@ public class Employee : IComparable<Employee>
     public string givenname;
     public string surname;
 
+    // multi-key sort will be judged based firstly on surname, followed by givenname
     public int CompareTo(Employee other)
     {
         if (this.surname.CompareTo(other.surname) > 0)
